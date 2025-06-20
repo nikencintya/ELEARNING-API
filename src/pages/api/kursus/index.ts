8d73c5a9c2b6b71fd8f678820b2fc4213fb4e12b
wandak22 <wandakhairunisa329@gmail.com>
2025-06-20 20:40:00 +0700

    terbaru

--- src/pages/api/kursus/index.ts
+++ src/pages/api/kursus/index.ts
@@ -1,26 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import clientPromise from '../../lib/mongodb'
- 
-type ResponseData = {
-  message: string
-}
- 
+import clientPromise from "../../../lib/mongodb";
+
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<ResponseData>
+  res: NextApiResponse
 ) {
-  const client = await clientPromise;
+const client = await clientPromise;
   const db = client.db("elearning");
-  {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  }
-
   switch (req.method) {
     case "GET":
-      const allPosts = await db.collection("kursus").find({}).toArray();
-      res.status(200).json({ status: 200, data: allPosts });
+      const kursus = await db.collection("kursus").find({}).toArray();
+      res.json({ status: 200, data: kursus });
       break;
   }
 }
